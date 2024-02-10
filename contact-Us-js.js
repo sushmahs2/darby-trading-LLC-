07649425ecbe6ed648aa08cf5dde605af34404c9
sushmahs2 <71074130+sushmahs2@users.noreply.github.com>
2024-02-10 10:46:09 +0530

    Add files via upload

---
+++ contact-Us-js.js
@@ -0,0 +1,19 @@
+document.addEventListener('DOMContentLoaded', function () {
+
+    // menu button
+    const menuButton = document.getElementById('menuButton1');
+    const menu = document.getElementById('menu1');
+  
+
+    menuButton.addEventListener('click', function () {
+        menu.style.display = (menu.style.display === 'block') ? 'none' : 'block';
+        document.body.style.overflow = (menu.style.display === 'block') ? 'hidden' : 'auto';
+    });
+
+    document.addEventListener('click', function (event) {
+        if (!menuButton.contains(event.target) && !menu.contains(event.target)) {
+            menu.style.display = 'none';
+            document.body.style.overflow = 'auto';
+        }
+    });
+});
